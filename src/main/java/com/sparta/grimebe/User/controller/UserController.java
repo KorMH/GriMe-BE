ca79b2ab34cc8ebbf90424bce684aed746b0b32e
MyungHyun Lee <dvkk12@gmail.com>
2023-07-17 02:05:42 +0900

    "회원가입 로그인" Filter 적용, PasswordConfig -> WebSecurityConfig 변경, 불필요한 User Controller(login) 부분 삭제, UserService(login) 부분 삭제, UserDetailsImpl(Collection) 부분 코드 추가

--- src/main/java/com/sparta/grimebe/User/controller/UserController.java
+++ src/main/java/com/sparta/grimebe/User/controller/UserController.java
@@ -1,10 +1,9 @@
 package com.sparta.grimebe.User.controller;
 
-import com.sparta.grimebe.User.dto.LoginRequestDto;
-import com.sparta.grimebe.User.dto.ResponseDto;
 import com.sparta.grimebe.User.dto.SignupRequestDto;
 import com.sparta.grimebe.User.jwt.JwtUtil;
 import com.sparta.grimebe.User.service.UserService;
+import com.sparta.grimebe.global.BaseResponseDTO;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,25 +22,14 @@ public class UserController {
 
     // User Signup
     @PostMapping("/signup")
-    public ResponseEntity<ResponseDto> signUp(@Valid @RequestBody SignupRequestDto requestDto) {
+    public ResponseEntity<BaseResponseDTO> signUp(@Valid @RequestBody SignupRequestDto requestDto) {
 
         try {
             userService.signup(requestDto);
         } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(new ResponseDto("중복된 username 입니다.", HttpStatus.BAD_REQUEST.value()));
+            return ResponseEntity.badRequest().body(new BaseResponseDTO("중복된 username 입니다.", HttpStatus.BAD_REQUEST.value()));
         }
 
-        return ResponseEntity.status(201).body(new ResponseDto("회원가입 성공", HttpStatus.CREATED.value()));
-    }
-
-    //로그인
-    @PostMapping("/login")
-    public ResponseEntity<ResponseDto> login(@RequestBody SignupRequestDto loginRequestDto, HttpServletResponse response) {
-        try {
-            userService.login(loginRequestDto, response);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(new ResponseDto("회원을 찾을 수 없습니다.", HttpStatus.BAD_REQUEST.value()));
-        }
-        return ResponseEntity.ok().body(new ResponseDto("로그인 성공", HttpStatus.CREATED.value()));
+        return ResponseEntity.status(201).body(new BaseResponseDTO("회원가입 성공", HttpStatus.CREATED.value()));
     }
 }
--- src/main/java/com/sparta/grimebe/User/service/UserService.java
+++ src/main/java/com/sparta/grimebe/User/service/UserService.java
@@ -21,7 +21,6 @@ public class UserService {
 
     private final JwtUtil jwtUtil;
 
-    private final String ADMIN_TOKEN = "AAABnvxRVklrnYxKZ0aHgTBcXukeZygoC";
 
     public void signup(SignupRequestDto requestDto) {
         String username = requestDto.getUsername();
@@ -33,33 +32,9 @@ public class UserService {
 
         // 사용자 ROLE 확인
         UserRoleEnum role = UserRoleEnum.USER;
-        if (requestDto.isAdmin()) {
-            if (!ADMIN_TOKEN.equals(requestDto.getAdminToken())) {
-                throw new IllegalArgumentException("관리자 암호가 틀려 등록이 불가능합니다.");
-            }
-            role = UserRoleEnum.ADMIN;
-        }
-
         User user = new User(username, password, role);
 
         userRepository.save(user);
     }
 
-    public void login(SignupRequestDto requestDto, HttpServletResponse response) {
-        String username = requestDto.getUsername();
-        String password = requestDto.getPassword();
-
-        //사용자 확인 (username 이 없는 경우)
-        User user = userRepository.findByUsername(username).orElseThrow(
-                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
-        );
-
-        //비밀번호 확인 (password 가 다른 경우)
-        if(!passwordEncoder.matches(password, user.getPassword())) {
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
-        }
-
-        //JWT 생성 및 쿠키에 저장 후 Response 객체에 추가
-        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(user.getUsername(), user.getRole()));
-    }
 }
