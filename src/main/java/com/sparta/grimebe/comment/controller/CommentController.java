e9525824a606f36224987dcff50a508d5ceddc8d
junghwan1106 <11wjdfk@naver.com>
2023-07-17 13:12:49 +0900

    댓글 시큐리티 , 예외처리 수정1

--- src/main/java/com/sparta/grimebe/comment/controller/CommentController.java
+++ src/main/java/com/sparta/grimebe/comment/controller/CommentController.java
@@ -1,10 +1,12 @@
 package com.sparta.grimebe.comment.controller;
 
+import com.sparta.grimebe.User.security.UserDetailsImpl;
 import com.sparta.grimebe.comment.dto.CommentRequestDto;
 import com.sparta.grimebe.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -14,8 +16,8 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping
-    public ResponseEntity<?> createComment(@RequestHeader("Authorization") String token, @RequestBody CommentRequestDto requestDto) {
-        return new ResponseEntity<>(commentService.createComment(requestDto, token), HttpStatus.CREATED);
+    public ResponseEntity<?> createComment(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody CommentRequestDto requestDto) {
+        return new ResponseEntity<>(commentService.createComment(requestDto, userDetails), HttpStatus.CREATED);
     }
 
     @GetMapping
@@ -29,13 +31,13 @@ public class CommentController {
     }
 
     @PutMapping("/{commentId}")
-    public ResponseEntity<?> updateComment(@RequestHeader("Authorization") String token, @PathVariable Long commentId, @RequestBody CommentRequestDto requestDto) {
-        return new ResponseEntity<>(commentService.updateComment(commentId, requestDto, token), HttpStatus.OK);
+    public ResponseEntity<?> updateComment(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long commentId, @RequestBody CommentRequestDto requestDto) {
+        return new ResponseEntity<>(commentService.updateComment(commentId, requestDto, userDetails), HttpStatus.OK);
     }
 
     @DeleteMapping("/{commentId}")
-    public ResponseEntity<?> deleteComment(@RequestHeader("Authorization") String token, @PathVariable Long commentId) {
-        commentService.deleteComment(commentId, token);
+    public ResponseEntity<?> deleteComment(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long commentId) {
+        commentService.deleteComment(commentId, userDetails);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
