85af9880339945ff3fbc586ac9371382f2eb0966
Dsys1129 <dsys1129@gmail.com>
2023-07-18 12:27:09 +0900

    fix/comment : CommentResponseDto 필드 삭제

--- src/main/java/com/sparta/grimebe/comment/dto/CommentResponseDto.java
+++ src/main/java/com/sparta/grimebe/comment/dto/CommentResponseDto.java
@@ -1,24 +1,18 @@
 package com.sparta.grimebe.comment.dto;
 
 import com.sparta.grimebe.comment.entity.Comment;
+
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
-@NoArgsConstructor
 public class CommentResponseDto {
     private Long id;
     private String content;
     private String userName;
-    private String postTitle;
 
     public CommentResponseDto(Comment comment) {
         this.id = comment.getId();
         this.content = comment.getContent();
         this.userName = comment.getUser().getUsername();
-        this.postTitle = comment.getPost().getTitle();
     }
-
 }
